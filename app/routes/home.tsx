9ea8c100a7870a9df9a52a48cb0df658c6cbaf4e
Adrian <adrian@theround.com>
2025-07-25 09:32:59 +0100

    implement homepage, navbar and cv card

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,35 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
+import CVCard from "~/components/CVCard";
+import { resumes } from "../../constants";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "CV Engine" },
+    { name: "description", content: "Smart feedback for your CV" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover bg-center bg-no-repeat">
+      <Navbar />
+      <section className="main-section">
+        <div className="page-heading py-16">
+          <h1>
+            Track Your Applications &<br className="max-sm:hidden" /> CV Ratings
+          </h1>
+          <h2>Review your submissions and get AI-powered feedback.</h2>
+        </div>
+      </section>
+
+      {resumes.length > 0 && (
+        <div className="resumes-section mx-auto">
+          {resumes.map((resume: Resume) => (
+            <CVCard key={resume.id} resume={resume} />
+          ))}
+        </div>
+      )}
+    </main>
+  );
 }

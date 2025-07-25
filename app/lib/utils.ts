396683e173df4333a332070e3469fcece5b7b44d
Adrian <adrian@theround.com>
2025-07-25 23:26:50 +0100

    implemented cloud storage, pdf-image conversion, key-value storage and AI feedback

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,3 +1,10 @@
+import {type ClassValue, clsx} from "clsx";
+import {twMerge} from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
 export const formatSize = (bytes: number): string => {
   if (bytes === 0) return "0 Bytes";
 
@@ -7,3 +14,5 @@ export const formatSize = (bytes: number): string => {
 
   return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + " " + sizes[i];
 };
+
+export const generateUUID = () => crypto.randomUUID();
--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -1,14 +1,95 @@
+import { prepareInstructions } from "constants";
 import React, { useState } from "react";
+import { useNavigate } from "react-router";
 import FileUploader from "~/components/FileUploader";
 import Navbar from "~/components/Navbar";
+import { convertPdfToImage } from "~/lib/pdf2img";
+import { usePuterStore } from "~/lib/puter";
+import { generateUUID } from "~/lib/utils";
 
 const upload = () => {
+  const { auth, isLoading, fs, ai, kv } = usePuterStore();
+  const navigate = useNavigate();
   const [isProcessing, setIsProcessing] = useState(false);
   const [statusText, setStatusText] = useState("");
   const [file, setFile] = useState<File | null>(null);
 
   const handleFileSelect = (file: File | null) => {
     setFile(file);
+  };
+
+  const handleAnalyse = async ({
+    companyName,
+    jobTitle,
+    jobDescription,
+    file,
+  }: {
+    companyName: string;
+    jobTitle: string;
+    jobDescription: string;
+    file: File;
+  }) => {
+    setIsProcessing(true);
+    setStatusText("Uploading your CV...");
+
+    const uploadedFile = await fs.upload([file]);
+    if (!uploadedFile) return setStatusText("Failed to upload your CV");
+
+    setStatusText("Converting your CV to image...");
+    const imageFile = await convertPdfToImage(file);
+
+    if (!imageFile.file)
+      return setStatusText("Failed to convert your CV to image");
+
+    setStatusText("Uploading your CV image...");
+    const uploadedImage = await fs.upload([imageFile.file]);
+    if (!uploadedImage) return setStatusText("Failed to upload your CV image");
+
+    setStatusText("Preparing your CV for analysis...");
+
+    const uuid = generateUUID();
+
+    const data = {
+      id: uuid,
+      cvPath: uploadedFile.path,
+      imagePath: uploadedImage.path,
+      companyName,
+      jobTitle,
+      jobDescription,
+      feedback: "",
+      createdAt: new Date().toISOString(),
+      updatedAt: new Date().toISOString(),
+    };
+
+    await kv.set(`CV:${uuid}`, JSON.stringify(data));
+
+    setStatusText("Analysing your CV...");
+
+    const feedback = await ai.feedback(
+      uploadedFile.path,
+      prepareInstructions({
+        jobTitle,
+        jobDescription,
+      })
+    );
+
+    if (!feedback) return setStatusText("Error: Failed to analyse your CV");
+
+    const feedbackText =
+      typeof feedback.message.content === "string"
+        ? feedback.message.content
+        : feedback.message.content[0].text;
+
+    data.feedback = JSON.parse(feedbackText);
+
+    await kv.set(`CV:${uuid}`, JSON.stringify(data));
+
+    setStatusText(
+      "Your CV analysis is complete, redirecting to feedback page..."
+    );
+    console.log(data);
+    // setIsProcessing(false);
+    // navigate(`/feedback/${uuid}`);
   };
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -23,13 +104,20 @@ const upload = () => {
     const jobTitle = formData.get("job-title") as string;
     const jobDescription = formData.get("job-description") as string;
 
-    console.log({ companyName, jobTitle, jobDescription, file });
+    // console.log({ companyName, jobTitle, jobDescription, file });
 
     if (!companyName || !jobTitle || !jobDescription || !file) {
       setStatusText("Please fill in all fields and upload a CV");
       setIsProcessing(false);
       return;
     }
+
+    handleAnalyse({
+      companyName,
+      jobTitle,
+      jobDescription,
+      file,
+    });
   };
   return (
     <main className="bg-[url('/images/bg-main.svg')] bg-cover bg-center bg-no-repeat">
